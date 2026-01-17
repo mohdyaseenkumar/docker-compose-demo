119d1691f7044474d6df42d237d80fb998b39f00
mohdyaseenkumar <53486897+mohdyaseenkumar@users.noreply.github.com>
2026-01-17 23:22:22 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,23 @@
 
+const mysql = require('mysql2');
+const express = require('express');
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: 'db',
+  user: 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: 'testdb'
+});
+
+app.get('/', (req, res) => {
+  db.query('SELECT "Docker Compose is working!" AS message', (err, result) => {
+    if (err) return res.send(err);
+    res.send(result);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
